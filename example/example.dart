f660db5ee714d771b008da8a3ff64f7cace01952
himmatsingh019 <himmatsingh19k@gmail.com>
2024-01-03 16:04:00 +0530

    flutter upgrade changes

--- example/example.dart
+++ example/example.dart
@@ -38,6 +38,7 @@ class Home extends StatelessWidget {
                 controller: controller,
                 storyItems: [
                   StoryItem.text(
+                    storyId: 0,
                     title:
                         "Hello world!\nHave a look at some great Ghanaian delicacies. I'm sorry if your mouth waters. \n\nTap!",
                     backgroundColor: Colors.orange,
@@ -56,6 +57,7 @@ class Home extends StatelessWidget {
                   //   ),
                   // ),
                   StoryItem.inlineImage(
+                    storyId: 0,
                     url:
                         "https://image.ibb.co/cU4WGx/Omotuo-Groundnut-Soup-braperucci-com-1.jpg",
                     controller: controller,
@@ -69,6 +71,7 @@ class Home extends StatelessWidget {
                     ),
                   ),
                   StoryItem.inlineImage(
+                    storyId: 0,
                     url:
                         "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
                     controller: controller,
@@ -154,10 +157,12 @@ class _MoreStoriesState extends State<MoreStories> {
       body: StoryView(
         storyItems: [
           StoryItem.text(
+            storyId: 0,
             title: "I guess you'd love to see more of our food. That's great.",
             backgroundColor: Colors.blue,
           ),
           StoryItem.text(
+            storyId: 0,
             title: "Nice!\n\nTap to continue.",
             backgroundColor: Colors.red,
             textStyle: TextStyle(
@@ -166,21 +171,25 @@ class _MoreStoriesState extends State<MoreStories> {
             ),
           ),
           StoryItem.pageImage(
+            storyId: 0,
             url:
                 "https://image.ibb.co/cU4WGx/Omotuo-Groundnut-Soup-braperucci-com-1.jpg",
             caption: "Still sampling",
             controller: storyController,
           ),
           StoryItem.pageImage(
+              storyId: 0,
               url: "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
               caption: "Working with gifs",
               controller: storyController),
           StoryItem.pageImage(
+            storyId: 0,
             url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
             caption: "Hello, from the other side",
             controller: storyController,
           ),
           StoryItem.pageImage(
+            storyId: 0,
             url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
             caption: "Hello, from the other side2",
             controller: storyController,
--- lib/widgets/story_image.dart
+++ lib/widgets/story_image.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-import '../utils.dart';
 import '../controller/story_controller.dart';
+import '../utils.dart';
 
 /// Utitlity to load image (gif, png, jpg, etc) media just once. Resource is
 /// cached to disk with default configurations of [DefaultCacheManager].
@@ -44,9 +44,11 @@ class ImageLoader {
 
         final imageBytes = fileResponse.file.readAsBytesSync();
 
+        ImmutableBuffer buffer = imageBytes as ImmutableBuffer;
+
         this.state = LoadState.success;
 
-        PaintingBinding.instance!.instantiateImageCodec(imageBytes).then(
+        PaintingBinding.instance.instantiateImageCodecWithSize(buffer).then(
             (codec) {
           this.frames = codec;
           onComplete();
